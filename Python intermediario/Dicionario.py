1216ad93d4372229977723ca625a4b7c2a72f6c2
Kevyn <kevynsilveira0610@gmail.com>
2023-07-19 20:25:04 -0300

    Navegando no dicionario

---
+++ Python intermediario/Dicionario.py
@@ -0,0 +1,13 @@
+# Criando um dicionario
+
+# Um dicionario e identificado pois utiliza chaves na sua criacao e tem um padrao
+# para criacao da seguinte maneira primeiro indica o valor a ser armazena, cooloque
+# dois pontos : e sem seguida entre aspas tambem coloque o valor a ser armazenado dentro
+# daquele indique adicionado anteriormente
+
+meu_dicionario = {"A":"Ameixa", "B":"Bola", "C":"Casa"}
+
+
+# Chama o print e passa o indice que deseja chamar
+
+print(meu_dicionario["B"])
