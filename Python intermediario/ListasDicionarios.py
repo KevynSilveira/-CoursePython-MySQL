1ead53b6737e591e9560485b38b2d9eaefc63f06
Kevyn <kevynsilveira0610@gmail.com>
2023-07-18 23:29:59 -0300

    Acompanhando os codigos do curso na aula de dicionarios e listas

--- Python intermediario/ListasDicionarios.py
+++ Python intermediario/ListasDicionarios.py
@@ -9,3 +9,15 @@ print(numerica, "\n", palavras)
 
 #comeca no 0, entao o retorno sera 5
 print(numerica[4])
+
+#navegar entre na lista
+for item in numerica:
+    print(item)
+
+#pega o tamanho da lista
+tamanho = len(numerica)
+print(tamanho)
+
+#Para adicionar conteudo a lista:
+numerica.append(11)
+print(numerica)
