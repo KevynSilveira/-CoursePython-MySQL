7409c6dc8e1dde99d5a957d995f38fed9206eed2
Kevyn <kevynsilveira0610@gmail.com>
2023-07-19 20:56:53 -0300

    implementado a funcao write com o parametro w para adicionar um novo arquivo ou apagar o ja existente com o mesmo nome.

--- Python intermediario/ManipulacaoArquivo.py
+++ Python intermediario/ManipulacaoArquivo.py
@@ -18,10 +18,10 @@ meu_arquivo2.close()
 
 # Cria um novo arquivo de texto com o nome Meu arquivo3, ou se ja tiver um existente com esse nome
 # ele apaga tudo e deixa em branco
-meu_arquivo3 = open("Meu arquivo3.txt", "w")
+meu_arquivo3 = open("Meu arquivo3.txt", "a")
 
 #Escreve no arquivo aberto
-meu_arquivo2.write("Essa e a cricao de um novo arquivo de texto utilizando a funcao wirte")
+meu_arquivo3.write("Essa e a cricao de um novo arquivo de texto utilizando a funcao write")
 
 # Fecha o arquivo de texto aberto
 meu_arquivo3.close()
