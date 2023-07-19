43b738ba02d54d73f0ec29b9faa233de9a131d2a
Kevyn <kevynsilveira0610@gmail.com>
2023-07-19 20:45:42 -0300

    Adicionando o arquivo de texto utilizado para manipulacao e adicionando um exemplo da implementacao da funcao readlines

---
+++ Python intermediario/ManipulacaoArquivo.py
@@ -0,0 +1,10 @@
+# Abre o arquivo para manipulacao e armazena dentro da variavel
+# meu_arquivo
+meu_arquivo = open("meu arquivo.txt")
+
+# Le as linhas e adiciona na variavel linha em formato de lista
+linhas = meu_arquivo.readlines()
+
+# Exibe a lista
+print(linhas)
+
