e0e457606b93a4a2ba47b294062361e58682adf6
Kevyn <kevynsilveira0610@gmail.com>
2023-07-19 20:48:20 -0300

    Adicionando a funcao read onde le o arquivo inteiro e armazena em apenas uma variavel

--- Python intermediario/ManipulacaoArquivo.py
+++ Python intermediario/ManipulacaoArquivo.py
@@ -1,10 +1,13 @@
-# Abre o arquivo para manipulacao e armazena dentro da variavel
-# meu_arquivo
-meu_arquivo = open("meu arquivo.txt")
+# Abre o arquivo para manipulacao e armazena dentro da variavel meu_arquivo1
+meu_arquivo1 = open("meu arquivo.txt")
 
 # Le as linhas e adiciona na variavel linha em formato de lista
-linhas = meu_arquivo.readlines()
-
-# Exibe a lista
+linhas = meu_arquivo1.readlines()
 print(linhas)
 
+# Abre o arquivo para manipulacao e armazena dentro da variavel meu_arquivo2
+meu_arquivo2 = open("Meu arquivo.txt")
+
+#armazena o texto completo dentro da variavel
+texto_completo = meu_arquivo2.read()
+print(texto_completo)
