a862972d136cb894ddaa0231b83daf66a63ed2b2
Kevyn <kevynsilveira0610@gmail.com>
2023-07-18 23:26:26 -0300

    Acompanhando os codigos do curso na aula de dicionarios e listas

---
+++ Python intermediario/ListasDicionarios.py
@@ -0,0 +1,11 @@
+#Lista representa conjunto de dados em python
+
+#numerica
+numerica = [1,2,3,4,5,6,7,8,9,10]
+palavras = ["nome", "idade", "endereco"]
+misturada = ["nome", "idade", "endereco", 1, 2.3, 4, 5.6]
+
+print(numerica, "\n", palavras)
+
+#comeca no 0, entao o retorno sera 5
+print(numerica[4])
