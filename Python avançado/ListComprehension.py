be17a6dbca78dc95626df520489cd0e35adf1329
Kevyn <kevynsilveira0610@gmail.com>
2023-07-20 20:55:10 -0300

    Adicionando ListComprehension exemplo de Adicionando ListComprehension

---
+++ Python avançado/ListComprehension.py
@@ -0,0 +1,9 @@
+# Exemplo de codigo sem listComprehension
+x = [1, 2, 3, 4] # Cria uma lista
+y = []
+
+for i in x: # Faz um for para passar em todos as variavel da lista e elevar elas a 2
+    y.append(i**2)
+
+print(x)
+print(y)
