614555f159c0cee013aa883ceff33f8aec157fe7
Kevyn <kevynsilveira0610@gmail.com>
2023-07-19 21:37:19 -0300

    Adicionado o exercicio 2 de abrir arquivo solicitado pelo usuario

---
+++ Python intermediario/ComparaSequencia.py
@@ -0,0 +1,7 @@
+seq1 = input("Digite uma sequencia:")
+seq2 = input("Digite uma sequencia:")
+
+if seq1 == seq2:
+     print("Sequencia esta correta!")
+else:
+    print("A sequencia nao sao iguais!")
