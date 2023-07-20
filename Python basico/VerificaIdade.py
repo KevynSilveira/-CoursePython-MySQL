ebf10d6f67e50f441d226407c48d2629678c31e5
Kevyn <kevynsilveira0610@gmail.com>
2023-07-20 20:48:05 -0300

    Adicionando codigos

--- Python basico/VerificaIdade.py
+++
@@ -1,7 +0,0 @@
-nome = input("Qual seu nome? ")
-idade = int(input("Qual a sua idade? "))
-
-if idade >= 18:
-    print(nome + ",você é maior de idade!")
-else:
-    print(nome + ", você é menor de idade!")
--- Python intermediario/ComparaSequencia.py
+++
@@ -1,7 +0,0 @@
-seq1 = input("Digite uma sequencia:")
-seq2 = input("Digite uma sequencia:")
-
-if seq1 == seq2:
-     print("Sequencia esta correta!")
-else:
-    print("A sequencia nao sao iguais!")
