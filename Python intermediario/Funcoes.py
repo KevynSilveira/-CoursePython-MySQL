d82170e1eefa12ae6391e95ac0db8b64864ffbca
Kevyn <kevynsilveira0610@gmail.com>
2023-07-19 21:01:50 -0300

    Novo codigo de funcao, com ja implementado a funcao de soma

---
+++ Python intermediario/Funcoes.py
@@ -0,0 +1,10 @@
+# Criando uma funcao de soma
+def soma(a,b):
+    x = a + b
+
+# Retorna o valor de x
+    return x
+
+# Pega o valor retornado e salva em uma variavel resultado
+resultado = soma(777, 223)
+print(resultado)
