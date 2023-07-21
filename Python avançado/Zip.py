efdba5dcfef7888befc6927f36f09e373cefd3a2
Kevyn <kevynsilveira0610@gmail.com>
2023-07-20 21:18:32 -0300

    Finalizacao do zip

---
+++ Python avançado/Zip.py
@@ -0,0 +1,7 @@
+# Exemplo de Zip
+
+lista1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+lista2 = ["abacaxi", "bola", "Cachorro", "Dado", "Elefante"]
+
+for numero, nome in zip(lista1, lista2): # Concatena duas lista, podendo ser concatenada mais de duas lista
+    print(numero, nome)
