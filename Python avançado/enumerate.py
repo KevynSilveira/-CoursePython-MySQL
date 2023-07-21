65f86a18a948eb660e0721a07c94380a847d28d4
Kevyn <kevynsilveira0610@gmail.com>
2023-07-20 21:05:38 -0300

    Cria um exemplo de um codigo sem enumerate

--- Python avançado/enumerate.py
+++ Python avançado/enumerate.py
@@ -4,3 +4,8 @@ lista = ["abacaxi", "bola", "Cachorro"] # Cria a lista com o conteudo
 
 for i in range (len(lista)): # Lista os elementos e os indices a qual pertence
     print(i, lista[i])
+
+# Exemplo com enumerate
+
+for i , nome in enumerate(lista): # Exemplo de codigo utilizando enumerate
+    print(i, nome)
