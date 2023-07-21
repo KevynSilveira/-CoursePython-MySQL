e1fa14b59d91aa5b49863e56897caf8c53d1bc83
Kevyn <kevynsilveira0610@gmail.com>
2023-07-20 21:03:30 -0300

    Cria um exemplo de enumerate

---
+++ Python avançado/enumerate.py
@@ -0,0 +1,6 @@
+# Exemplo sem enumerate
+
+lista = ["abacaxi", "bola", "Cachorro"] # cria a lista com o conteudo
+
+for i in range (len(lista)): # Lista os elementos e os indices a qual pertence
+    print(i, lista[i])
